e0b077cef360fe0f0b5f21e5f35c73c157aee8f0
alieksandrova1 <43207130+alieksandrova1@users.noreply.github.com>
2018-10-07 23:39:12 +0300

    Update laba_3.c

--- laba_3.c
+++ laba_3.c
@@ -1,79 +1,43 @@
 #include <stdio.h>
-#define SIZE 1000
 int main(void)
 {
-    char str1[SIZE];
-    char str2[SIZE];
-    char pr[SIZE];
+    char str[80];
+    int i = 0, j, k = 0, m, flag = 0, xflag = 0;
     
+    printf("Input line\n");
+    fgets(str, 80, stdin);
+    char *s = str;
     
-    int cpr = 0;
-    int counter = 0;
-    int p, i, j, n;
-    int len2;
-    
-    printf("Enter the string: \n");
-    fgets(str1, SIZE, stdin);
-    int len = strlen(str1);
-    str1[len] = ' ';
-    char *s = str1;
-
- 
-    
-    printf("Enter the prist: \n");
-    fgets(pr, SIZE, stdin);
-    
-    int prlen = strlen(pr);
-    char *h = pr;
-    
-    char *s2 = str2;
-    
-    for(int i = 0; i <= len; i++)
+    while(*(s+i))
     {
-        if(((*(s+i) >= '0')&&(*(s+i) <= '9'))||((*(s+i-1) >= '0')&&(*(s+i-1) <= '9')))
+        if(*(s+i) != ' ' && flag == 0)
         {
-            counter = 0;
-            continue;
+            flag = 1;
+            k = i;
         }
-        else if((*(s+i) != ' ')&&(*(s+i) != '.')&&(*(s+i) != ','))
-            counter++;
-        else
+        if(flag == 1 && (*(s+i) == ' ' || *(s+i) == '.' || *(s+i) == '\n' || *(s+i) == ','))
         {
-            
-            for(p = i - counter, j = 0; j < prlen; j++, p++)
+            for(j = k; j < i - 1; j++)
             {
-                if(*(s+p) == *(h+j))
-                    cpr++;
-                else
-                    break;
+                for(m = k + 1; m < i; m++)
+                {
+                    if(*(s+j) == *(s+m) && j != m)
+                    {
+                        xflag = 1;
+                    }
+                }
             }
             
-            if(cpr == prlen)
+            if(xflag == 0)
             {
-                for(p = i - counter, j = 0; j < counter; j++, p++)
-                    *(s+p) = ' ';
+                for(j = k; j < i; j++)
+                    printf("%c",*(s+j));
+                printf(" ");
             }
-            
-            cpr = 0;
-            counter = 0;
+            xflag = 0;
+            flag = 0;
         }
+        i++;
     }
-    
-    for( i = 0, j =0; i < len; i++)
-    {
-        if((*(s+i) == ' ') && (*(s+i+1) == ' '))
-            continue;
-        *(s2+j) = *(s+i);
-        j++;
-    }
-    
-    len2 = strlen(str2);
-    
-    for(int i = 0; i < len2; i++)
-    {
-        printf("%c", *(s2+i));
-    }
-    
     return 0;
 }
-
